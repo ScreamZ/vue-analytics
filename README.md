08015cfdea88c066ec569c37b6d11a6b7cf48baf
Andréas Hanss <ahanss@kaliop.com>
2016-11-15 11:40:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,100 @@
+<h1 align="center">
+  Vue Analytics
+  <br>
+  <br>
+</h1>
+
+<h4 align="center">Simple implementation of Google Analytics in Vue.js</h4>
+
+<p align="center">
+  <a href="https://github.com/feross/standard"><img src="https://cdn.rawgit.com/feross/standard/master/badge.svg" alt="Standard - JavaScript Style Guide"></a>
+</p>
+<br>
+
+Dispatch events and track views from Vue components.
+
+# Requirements
+
+- **Vue.js.** > 2.0.0
+- **Google Analytics account.** To retrieve Data
+
+
+# Configuration
+
+Just do the following, the configuration of dimensions is optionnal.
+
+```javascript
+import VueAnalytics from 'vue-analytics'
+
+Vue.use(VueAnalytics, {
+  appName: '<app_name>',
+  trackingId: '<your_tracking_id>',
+  debug: true, // Wheter or not display console logs
+  globalDimensions: [
+    {dimension: 1, value: 'MyDimensionValue'},
+    {dimension: 2, value: 'AnotherDimensionValue'}
+  ]
+})
+```
+
+# Documentation
+
+Once registered you can access vue analytics in your components like this :
+
+```javascript
+export default {
+    name: 'MyComponent',
+    data () {
+      return {
+        someData: false
+      }
+    },
+    methods: {
+      onClick: function() {
+        this.$ua.trackEvent('Banner', 'Click', 'I won money!')
+      }
+    },
+    mounted () {
+      this.$ua.trackView('MyScreenName')
+    }
+}
+```
+
+You can also access the instance everywhere using `window.vueAnalytics`, it's useful when you are in the store or somewhere else than components.
+
+## API reference
+
+### injectGlobalDimension (dimensionNumber, value)
+```javascript
+  /**
+   * Inject a new GlobalDimension that will be sent every time.
+   *
+   * Prefer inject through plugin configuration.
+   *
+   * @param {int} dimensionNumber
+   * @param {string|int} value
+   */
+```
+
+### trackEvent (category, action = null, label = null, value = null, fieldsObject = {})
+```javascript
+  /**
+   * Dispatch an analytics event.
+   * Format is the same as analytics classical values.
+   *
+   * @param category
+   * @param action
+   * @param label
+   * @param value
+   * @param fieldsObject
+   */
+```
+
+### trackView (screenName)
+```javascript
+ /**
+   * Dispatch a view using the screen name
+   * 
+   * @param screenName
+   */
+```
---
+++ analytics.js
@@ -0,0 +1,96 @@
+export default class AnalyticsPlugin {
+  constructor (conf) {
+    this.conf = conf
+  }
+
+  trackView (screenName) {
+    if (this.conf.debug) {
+      console.log('Dispatching TrackView', {screenName})
+    }
+
+    ga('send', 'screenview', {screenName})
+  }
+
+  /**
+   * Dispatch an analytics event
+   *
+   * @param category
+   * @param action
+   * @param label
+   * @param value
+   * @param fieldsObject
+   */
+  trackEvent (category, action = null, label = null, value = null, fieldsObject = {}) {
+    if (this.conf.debug) {
+      console.log('VueAnalytics : Dispatching event', {category, action, label, value, fieldsObject})
+    }
+
+    ga('send', 'event', category, action, label, value, fieldsObject)
+  }
+
+  /**
+   * Inject a new GlobalDimension that will be sent every time.
+   *
+   * Prefer inject through plugin configuration.
+   *
+   * @param {int} dimensionNumber
+   * @param {string|int} value
+   */
+  injectGlobalDimension (dimensionNumber, value)  {
+    if (this.conf.debug) {
+      console.log('VueAnalytics : Trying dimension Injection...', {dimensionNumber, value})
+    }
+
+    // Test if dimension already registered
+    if (this.conf.globalDimensions.find(el => el.dimension === dimensionNumber)) {
+      throw new Error('VueAnalytics : Dimension already registered')
+    }
+
+    // Otherwise add dimension
+    const newDimension = {dimension: dimensionNumber, value}
+
+    this.conf.globalDimensions.push(newDimension)
+    ga('set', `dimension${newDimension.dimension}`, newDimension.value)
+    console.log('VueAnalytics : Dimension injected')
+  }
+}
+
+AnalyticsPlugin.install = function (Vue, conf) {
+
+  if (!conf.trackingId) {
+    throw new Error('VueAnalytics : Please provide a "trackingId" from the config')
+  }
+
+  if (!conf.appName) {
+    throw new Error('VueAnalytics : Please provide a "appName" from the config')
+  }
+
+  // Declare analytics snipper
+  (function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+      }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+      m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+  })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+  // Register tracker
+  ga('create', conf.trackingId, 'auto')
+  ga('set', 'appName', conf.appName)
+
+  // Create global dimensions
+  if (conf.globalDimensions) {
+    conf.globalDimensions.forEach(dimension => {
+      ga('set', `dimension${dimension.dimension}`, dimension.value)
+    })
+  }
+
+  const analyticsPlugin = new AnalyticsPlugin(conf)
+
+  // Add to vue prototype and also window for access through store...
+  Vue.prototype.$ua = window.vueAnalytics = analyticsPlugin
+}
