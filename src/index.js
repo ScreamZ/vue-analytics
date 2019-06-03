e3c4f5c9ae23926c2404a05626e67cd6114207f3
Andréas HANSS <ahanss@kaliop.com>
2019-06-03 17:11:00 +0200

    Add feature usePathAnalytics.

--- src/index.js
+++ src/index.js
@@ -12,12 +12,12 @@ const install = function (Vue, initConf = {}) {
   /*eslint-disable*/
   // Load the analytics snippet
   (function (i, s, o, g, r, a, m) {
-    i[ 'GoogleAnalyticsObject' ] = r;
-    i[ r ] = i[ r ] || function () {
-        (i[ r ].q = i[ r ].q || []).push(arguments)
-      }, i[ r ].l = 1 * new Date();
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function () {
+      (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
     a = s.createElement(o),
-      m = s.getElementsByTagName(o)[ 0 ];
+      m = s.getElementsByTagName(o)[0];
     a.async = 1;
     a.src = g;
     m.parentNode.insertBefore(a, m)
@@ -83,8 +83,11 @@ const initVueRouterGuard = function (Vue, vueRouter, ignoredViews, trackPage) {
       return
     }
 
-    // Dispatch vue event using meta analytics value if defined otherwise fallback to route name
-    Vue.analytics.trackView(to.meta.analytics || to.name, trackPage)
+    // If specified : the system will use the path instead of the route name.
+    const viewName = to.meta.usePathAnalytics ? to.path : to.name
+
+    // Dispatch vue event using meta analytics value if defined otherwise fallback to route name/path.
+    Vue.analytics.trackView(to.meta.analytics || viewName, trackPage)
   })
 
   return ignoredViews
